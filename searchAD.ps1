57674f81e584046e3d4743735ac936ecfb0e1e21
reklovgit2020 <70860182+reklovgit2020@users.noreply.github.com>
2022-07-28 18:52:51 +0200

    Update searchAD.ps1

--- searchAD.ps1
+++ searchAD.ps1
@@ -1,16 +1,20 @@
-$days = 100
+$days = 1
 $windowsepochts = [datetime]::Now.ToFileTime()
-$comparedts =$windowsepochts-(86400000*days)
+$comparedts =$windowsepochts-(86400000*$days)
 $search = [adsisearcher]"(&(ObjectCategory=Person)(ObjectClass=User)(cn=*admin*)(lastlogontimestamp<=$comparedts))"
+#$search = [adsisearcher]"(&(ObjectCategory=Person)(ObjectClass=User)(lastlogontimestamp<=$comparedts))"
 $users = $search.FindAll()
 foreach($user in $users) {
     $CN = $user.Properties['CN']
     $DisplayName = $user.Properties['DisplayName']
     $SamAccountName = $user.Properties['SamAccountName']
-	  $groupMembership = $user.Properties['Membership']
+	$groupMembership = $user.Properties['Membership']
+	$lastlogon = $user.Properties['lastlogontimestamp']
+	#[datetime]::FromFileTime([int64][string]$ts)
+	$flastlogon = [datetime]::FromFileTime([int64][string]$lastlogon) 
 	#$user.Properties	
     "CN is $CN"
     "Display Name is $DisplayName"
     "SamAccountName is $SamAccountName"
-	"SamAccountName is $lastlogon"
+	"Last Logon was $flastlogon"
 }
