861d503914dcc7505b5b24e2af8e0f04a6eeff05
reklovgit2020 <70860182+reklovgit2020@users.noreply.github.com>
2022-09-23 13:56:38 +0200

    Update searchAD.ps1

--- searchAD.ps1
+++ searchAD.ps1
@@ -1,9 +1,18 @@
-$days = 1
-$windowsepochts = [datetime]::Now.ToFileTime()
-$comparedts =$windowsepochts-(86400000*$days)
+#bei 25 Tagen kommt er in die fliesskomma darstellung
+$days = 10
+[int64] $windowsepochts = [datetime]::Now.ToFileTime()
+[int64] $comparedts =$windowsepochts-(86400000*$days)
+"$windowsepochts - $comparedts"
+#users with admin name and without login since
 $search = [adsisearcher]"(&(ObjectCategory=Person)(ObjectClass=User)(cn=*admin*)(lastlogontimestamp<=$comparedts))"
+
+
+$search.SearchRoot = [ADSI]"LDAP://DC=<lorem>,DC=<lorem>"
+
 #$search = [adsisearcher]"(&(ObjectCategory=Person)(ObjectClass=User)(lastlogontimestamp<=$comparedts))"
 $users = $search.FindAll()
+$count = $users.count
+"Results is $count"
 foreach($user in $users) {
     $CN = $user.Properties['CN']
     $DisplayName = $user.Properties['DisplayName']
